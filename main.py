fa8a899942c023302d184e06cce2c659654dcbd8
snymanc <claytonsnyman@gmail.com>
2022-05-05 07:25:43 +0200

    HTTP Post request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -11,3 +12,9 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
