40a4507692a83eb9879dc950f715900bfe0210b8
snymanc <claytonsnyman@gmail.com>
2022-05-06 07:34:30 +0200

    1:52:46 Changing response Status Codes(status w/ message)

--- main.py
+++ main.py
@@ -49,4 +49,5 @@ def get_post(id: int, response: Response):
     post = find_post(id)
     if not post:
         response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": f"post with id: {id} was not found"}
     return {"post_detail": post}
