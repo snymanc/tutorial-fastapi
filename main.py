7f565728856799b73d9438e76dbef0e2d9a45299
snymanc <claytonsnyman@gmail.com>
2022-05-06 07:03:40 +0200

    Create Post 1:34:06

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -31,6 +32,7 @@ def get_posts():
 
 @app.post("/posts")
 def create_posts(post: Post):
-    print(post)
-    print(post.dict())
-    return {"data": "new post"}
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 10000000)
+    my_post.append(post_dict)
+    return {"data": post_dict}
