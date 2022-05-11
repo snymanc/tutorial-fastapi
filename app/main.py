8880dae5f5219cfb8aa32e6c9b4d23d2fb6e8ddb
snymanc <claytonsnyman@gmail.com>
2022-05-11 07:21:33 +0200

    4:08:00 DB CRUD - INSERT

--- app/main.py
+++ app/main.py
@@ -67,10 +67,11 @@ def get_posts():
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 10000000)
-    my_post.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
 
 @app.get("/posts/{id}")
