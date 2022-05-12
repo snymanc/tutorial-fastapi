33ea2b1ce38b35e3dc1639557a5d10f717450fe7
snymanc <claytonsnyman@gmail.com>
2022-05-12 08:13:36 +0200

    4:35:33 SQLALCHEMY setup - models

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from .database import Base
+
+
+class Post(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='True', nullable=False)
