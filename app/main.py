2bdeb6d27c9d589d765710576a1a1d7ab6c130ff
snymanc <claytonsnyman@gmail.com>
2022-05-20 08:07:43 +0200

    User - User Registration Path Operation

--- app/main.py
+++ app/main.py
@@ -112,3 +112,12 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     db.commit()
 
     return post_query.first()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_users(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
