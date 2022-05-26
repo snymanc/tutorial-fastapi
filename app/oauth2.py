a7bfe35e2b947f350c12176c5e97791d17bb6dbb
snymanc <claytonsnyman@gmail.com>
2022-05-26 07:57:28 +0200

    Authentication - Verify JWT

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,12 @@
-from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from . import schemas
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 
 SECRET_KEY = "5235d21776b207cd601b8a2f99fb1077aaa92cc1a5f94d7dda7ac1bbaf8ed62f"
 ALGORITHM = "HS256"
@@ -7,11 +14,69 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def create_access_token(data: dict):
+    """Generates a JWT
+
+    Args:
+        data (dict): Data to encode
+
+    Returns:
+        str: Returns a JWT token in a string
+
+    Examples:
+
+        >>> oauth2.create_access_token(data={"user_id": user.id})
+        "eyJhbGciOiJII6IkpXVCJ9.eyJhIjoiYiJ9.jiMyrsmD8AoHWeQgmxZ5yq8z0lXS67_QGs52AzC8Ru8"
+
+    """
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encode_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    """Decodes user JWT, raise exception when invalid
+
+    Args:
+        token (str): _description_
+        credentials_exception (_type_): _description_
+
+    Raises:
+        credentials_exception: Return HTTP 401 Status code
+
+    Returns:
+        str: Return the decode JWT payload
+
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    """Verifies current User with verify_access_token()
+
+    Args:
+        token (str, optional): User access_token. Defaults to Depends(oauth2_scheme).
+
+    Returns:
+        str: Token Data
+    """
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"})
+
+    return verify_access_token(token, credentials_exception)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -17,13 +17,13 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid Credentials"
         )
 
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid Credentials"
         )
 
