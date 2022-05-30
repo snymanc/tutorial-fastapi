4bb6a004969fd0bfc0e49f4395fe923dff60acb3
snymanc <claytonsnyman@gmail.com>
2022-05-30 07:37:18 +0200

    user_id - Assign user_id when creating new post

--- app/routers/post.py
+++ app/routers/post.py
@@ -19,8 +19,8 @@ def get_post(db: Session = Depends(get_db), current_user: int = Depends(oauth2.g
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),
-                 current_user: int = Depends(oauth2.get_current_user)):
-    new_post = models.Post(**post.dict())
+                current_user: int = Depends(oauth2.get_current_user)):
+    new_post = models.Post(user_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
