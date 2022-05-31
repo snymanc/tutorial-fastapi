dabd32fc04cab392e2f309f8fa9e945bf602b865
snymanc <claytonsnyman@gmail.com>
2022-05-31 07:18:32 +0200

    SQLAlchemy - relationship (https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html)

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -14,8 +15,10 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
-    user_id = Column(Integer,
-                     ForeignKey(column="users.id", ondelete="CASCADE"), nullable=False)
+    owner_id = Column(Integer,
+                      ForeignKey(column="users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User")
 
 
 class User(Base):
--- app/schemas.py
+++ app/schemas.py
@@ -3,25 +3,6 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
-class PostCreate(PostBase):
-    pass
-
-
-class Post(PostBase):
-    id: int
-    created_at: datetime
-    user_id: int
-
-    class Config:
-        orm_mode = True
-
-
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -39,6 +20,26 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
 
 
 class Token(BaseModel):
