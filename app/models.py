41f5956583aa335b8a2c1e00c1797b4ab03913aa
snymanc <claytonsnyman@gmail.com>
2022-06-01 08:29:17 +0200

    SQLAlchemy - Composite Keys

--- app/models.py
+++ app/models.py
@@ -28,3 +28,11 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False,
                         server_default=text('now()'))
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+    post_id = Column(Integer,
+                     ForeignKey(column="posts.id", ondelete="CASCADE"), primary_key=True)
+    user_id = Column(Integer,
+                     ForeignKey(column="users.id", ondelete="CASCADE"), primary_key=True)
