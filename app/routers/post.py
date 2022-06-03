ea8d2098a2ca619b5b67ad790990279928a807d3
snymanc <claytonsnyman@gmail.com>
2022-06-03 08:29:04 +0200

    SQLAlchemy - query joins with filters for single post

--- app/routers/post.py
+++ app/routers/post.py
@@ -40,14 +40,23 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),
     return new_post
 
 
-@router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+@router.get("/{id}", response_model=schemas.PostOut)
+def get_post(id: int,
+             db: Session = Depends(get_db),
+             current_user=Depends(oauth2.get_current_user)):
+
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).\
+        join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).\
+        group_by(models.Post.id).\
+        filter(models.Post.id == id).\
+        first()
+
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} was not found"
         )
+
     return post
 
 
