8d05fe2ef6b8d04eebeb992874f596f84bbefd44
snymanc <claytonsnyman@gmail.com>
2022-06-03 08:22:50 +0200

    SQLAlchemy - query joins with filters

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,27 +12,22 @@ router = APIRouter(
 )
 
 
-# @router.get("/")
 @router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db),
               current_user=Depends(oauth2.get_current_user),
               limit: int = 10,
               skip: int = 0,
               search: Optional[str] = ""):
-    posts = db.query(models.Post). \
+
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).\
+        join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).\
+        group_by(models.Post.id).\
         filter(models.Post.title.contains(search)). \
         limit(limit). \
         offset(skip). \
         all()
 
-    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).\
-        join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).\
-        group_by(models.Post.id).\
-        all()
-
-    print(results)
-    return results
-    # return posts
+    return posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
