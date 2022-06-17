e98e3b1e5171f0e9c85c7dc67d00485f6fc8c0ef
snymanc <claytonsnyman@gmail.com>
2022-06-17 09:17:29 +0200

    pytest - user - create user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from app import main, schemas
+from app import main
 
 client = TestClient(main.app)
 
@@ -9,3 +9,13 @@ def test_root():
     response = client.get("/")
     assert response.json().get("message") == "Hello World"
     assert response.status_code == 200
+
+
+def test_create_user():
+    response = client.post(
+        "/users/",
+        json={"email": "hello@email.com", "password": "password123"}
+    )
+
+    assert response.json().get("email") == "hello@email.com"
+    assert response.status_code == 201
