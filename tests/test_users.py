ab0d1d526ecedc7a082d2d366ec0a5be05348e66
snymanc <claytonsnyman@gmail.com>
2022-06-17 10:13:09 +0200

    pytest - user - import are database.py to access database session and FastAPI testclient client

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,21 +1,13 @@
-from fastapi.testclient import TestClient
 import pytest
 
-from app import main, schemas
-
-client = TestClient(main.app)
-
-
-def test_root():
-    response = client.get("/")
-    assert response.json().get("message") == "Hello World"
-    assert response.status_code == 200
+from app import schemas
+from .database import client, session
 
 
 @pytest.mark.parametrize("email, password", [
     ("hello@email.com", "password123")
 ])
-def test_create_user(email, password):
+def test_create_user(client, email, password):
     response = client.post(
         "/users/",
         json={"email": email, "password": password}
