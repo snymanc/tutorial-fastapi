695b1eabed0a6aadf23e041612f1203640064b28
snymanc <claytonsnyman@gmail.com>
2022-06-28 07:48:09 +0200

    pytest - user - test login

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,6 +4,12 @@ from app import schemas
 from .database import client, session
 
 
+def test_root(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json().get('message') == "Hello World"
+
+
 @pytest.mark.parametrize("email, password", [
     ("hello@email.com", "password123")
 ])
@@ -17,3 +23,12 @@ def test_create_user(client, email, password):
     new_user = schemas.UserOut(**response.json())
     assert new_user.email == email
     assert response.status_code == 201
+
+
+@pytest.mark.parametrize("email, password", [
+    ("hello@email.com", "password123")
+])
+def test_login(client, email, password):
+    response = client.get("/login",
+                           data={"username": email, "password": password})
+    assert response.status_code == 200
