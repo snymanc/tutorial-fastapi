7d9c6327f7317d3d6ed4441353b94b0ed9881100
snymanc <claytonsnyman@gmail.com>
2022-06-29 17:54:15 +0200

    pytest - posts - test_unauthorized_get_post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -24,3 +24,8 @@ def test_get_all_posts(authorized_client, test_posts):
 def test_unauthorized_user_get_all_posts(client, test_posts):
     response = client.get("/posts/")
     assert response.status_code == 401
+
+
+def test_unauthorized_get_post(client, test_posts):
+    response = client.get(f"/posts/{test_posts[0].id}")
+    assert response.status_code == 401
