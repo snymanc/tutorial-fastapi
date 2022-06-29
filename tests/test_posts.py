b97251112d4b4ff3596efce6bd064db291f8573d
snymanc <claytonsnyman@gmail.com>
2022-06-29 17:47:34 +0200

    pytest - posts - test_get_all_posts additional asserts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,7 +1,21 @@
+from app import schemas
+
 # args available via tests/conftest.py
 # if the args are not apps to the function
 # results may vary
+
+
 def test_get_all_posts(authorized_client, test_posts):
     response = authorized_client.get("/posts/")
 
+    def validate_posts(post):
+        return schemas.PostOut(**post)
+
+    posts_list = list(map(validate_posts, response.json()))
+
     assert response.status_code == 200
+    assert len(response.json()) == len(test_posts)
+    assert posts_list[0].Post.id == test_posts[0].id
+    assert posts_list[1].Post.title == test_posts[1].title
+    assert posts_list[2].Post.content == test_posts[2].content
+    assert posts_list[3].Post.owner_id == test_posts[3].owner_id
