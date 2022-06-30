b41faa7d6d3bec34ea29eee4eaefc238bdba9067
snymanc <claytonsnyman@gmail.com>
2022-06-30 08:41:49 +0200

    pytest - votes - test_vote_twice_post

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -1,5 +1,24 @@
+import pytest
+
+from app import models
+
+
+@pytest.fixture
+def test_vote(test_posts, session, test_user):
+    new_vote = models.Vote(post_id=test_posts[3].id, user_id=test_user["id"])
+    session.add(new_vote)
+    session.commit()
+
+
 def test_vote_on_post(authorized_client, test_posts):
     response = authorized_client.post("/vote/",
                                       json={"post_id": test_posts[3].id, "dir": 1})
 
     assert response.status_code == 201
+
+
+def test_vote_twice_post(authorized_client, test_posts, test_vote):
+    response = authorized_client.post("/vote/",
+                                      json={"post_id": test_posts[3].id, "dir": 1})
+
+    assert response.status_code == 409
